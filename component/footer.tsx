dd9851ae7e577f0813fb1ee767d68f167870279a
amirahyusof <mirez.data@gmail.com>
2025-05-15 11:08:32 +0800

    add new component

---
+++ component/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer(){
+  return (
+    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <p>Build with love, Amirah Y</p>
+    </footer>
+  )
+}
