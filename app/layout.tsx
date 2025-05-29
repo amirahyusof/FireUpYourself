2cd4583d41f7d73f14e99e4ed3db795feec65fee
Amirah Yusof <mirez.data@gmail.com>
2025-05-29 13:23:20 +0800

    modify main layout and page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const montserratSans = Montserrat({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
+        className={`${montserratSans.variable} ${geistMono.variable} antialiased min-h-screen bg-black`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 
+import { Button } from "@/components/ui/button";
 import React from "react";
-import Footer from "@/component/footer";
+import Link from "next/link";
+import Image from "next/image";
 
-import { Suspense } from "react";
-import QuoteWrapper from "@/component/randomQuote";
 
 export default function Main(){
   return (
-    <div className="flex flex-col bg-black min-h-screen p-24">
-      <Suspense fallback={<p>Loading quote...</p>}>
-        <QuoteWrapper />
-      </Suspense>
+    <div className="flex justify-center mx-auto mt-20 overflow-x-hidden max-w-md rounded-4xl h-[640px] bg-gradient-to-r from-yellow-400 to-indigo-500 transition-colors duration-300">
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          src="/placeholder.jpg"
+          width= {400}
+          height={400}
+          alt="Home Image"
+          className="object-cover border-2"
+        />
+        <Button className="mt-2 bg-yellow-400 hover:bg-yellow-500 text-indigo-900 font-semibold px-4 py-2 rounded-xl shadow-md transition">
+          <Link href="/quote">
+            Daily Spark
+          </Link>
+        </Button>
+      </div>
       
-      <Footer />
     </div>
   );
 }
