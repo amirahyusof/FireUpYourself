3813a0b4ae69022f35c3fcc94cd8257184fa5b49
Amirah Yusof <mirez.data@gmail.com>
2025-06-04 22:10:51 +0800

    add margin top

--- app/(page)/quote/page.tsx
+++ app/(page)/quote/page.tsx
@@ -5,8 +5,8 @@ import Greeting from "@/components/Greeting";
 
 export default function QuotePage(){
   return (
-    <div className="flex flex-col space-y-4 justify-center mx-auto mt-20 overflow-x-hidden max-w-md rounded-4xl h-[640px] bg-gradient-to-b from-yellow-400 to-indigo-500 transition-colors duration-300">
-      <div className="ml-6 items-left">
+    <div className="flex flex-col space-y-8 justify-center mx-auto mt-6 overflow-x-hidden max-w-md rounded-4xl h-[640px] bg-gradient-to-b from-yellow-400 to-indigo-500 transition-colors duration-300">
+      <div className="ml-6 items-left -mt-20">
         <Greeting />
       </div>
 
