f51bbeb22736d6bc9b2d9ffcb6855f8f98b59026
Amirah Yusof <mirez.data@gmail.com>
2025-06-04 22:10:29 +0800

    changes margin top and path directory

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -8,7 +8,7 @@ import FrontImage from "@/public/mainPhoto.png"
 
 export default function MainPage(){
   return (
-    <div className="flex justify-center mx-auto mt-20 overflow-hidden max-w-md rounded-4xl h-[640px] bg-gradient-to-r from-yellow-400 to-indigo-500 transition-colors duration-300">
+    <div className="flex justify-center mx-auto mt-6 overflow-hidden max-w-md rounded-4xl h-[640px] bg-gradient-to-r from-yellow-400 to-indigo-500 transition-colors duration-300">
       <div className="w-full overflow-hidden items-center justify-center">
         <div className="w-full relative flex-items justify-center">
           <Image
@@ -24,7 +24,7 @@ export default function MainPage(){
           <h1 className="text-2xl font-extrabold text-indigo-400">Spark Arise</h1>
           <p className="text-sm text-indigo-300 mb-4 font-bold">Let&apos;s get motivated everyday!</p>
           
-          <Link href="/main">
+          <Link href="/quote">
             <Button className="mt-2 z-20 bg-yellow-400 border border-indigo-400 cursor-pointer hover:bg-yellow-500 text-indigo-900 font-semibold px-4 py-2 rounded-xl shadow-md transition">
               Daily Spark
             </Button>
